06e72fe1cb147d31740b89c39314dcba8ccae981
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 13:58:03 +0300

    entry file

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Hello, Builder</h1>
+    <AddNew :schema="formSchema" :inputs="inputs" />
+    <SendForm :inputs="inputs" @send-form="submitForm" />
+    <Form method="POST" :inputs="inputs" :schema="formSchema" />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form";
+import AddNew from "./components/AddNew";
+import SendForm from "./components/SendForm";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      inputs: [],
+      formSchema: {
+        type: ["text", "password"],
+        placeholder: ["Username", "Password"],
+        fields: ["username", "password"]
+      }
+    };
+  },
+  components: { Form, AddNew, SendForm },
+  methods: {
+    submitForm() {
+      console.log("sending form data", this.inputs);
+    }
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
