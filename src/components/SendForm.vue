e7d0fa758c667df17379aac0271856701fc28c98
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 13:59:02 +0300

    SendForm component

---
+++ src/components/SendForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click.prevent="sendForm">Submut</button>
+</template>
+
+<script>
+export default {
+  name: "SendForm",
+  props: ["inputs"],
+  methods: {
+    sendForm() {
+      this.$emit("send-form");
+      console.log("emitted event");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
