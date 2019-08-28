a0e3536d6f22b7cdaad8f883ba147060dceff150
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 13:58:49 +0300

    AddNew component

---
+++ src/components/AddNew.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click.prevent="createForm">Add new</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddNew",
+  props: ["schema", "inputs"],
+  methods: {
+    createForm() {
+      const formObject = {};
+      const { fields } = this.schema;
+
+      const config = {
+        value: "",
+        writable: true,
+        enumerable: true,
+        configurable: true
+      };
+
+      fields.forEach(f => Object.defineProperty(formObject, f, config));
+      this.inputs.push(formObject);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
