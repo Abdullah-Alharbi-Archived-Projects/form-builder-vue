0b16c4e14169c2f8ae1c7903b8190f7ac7dd286a
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 13:58:30 +0300

    form component

---
+++ src/components/Form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="forms">
+    <br />
+    <div v-for="input in inputs" :key="input.id">
+      <form :method="method">
+        <div v-for="(type, typeIndex) in schema.type" :key="typeIndex">
+          <Input
+            :type="type"
+            :model="getPropName(input, typeIndex)"
+            :placeholder="schema.placeholder[typeIndex]"
+          />
+        </div>
+      </form>
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from "./Input";
+
+export default {
+  name: "Form",
+  props: ["method", "inputs", "schema"],
+  components: { Input },
+  methods: {
+    getPropName(object, index) {
+      const props = Object.keys(object);
+      return { object, props, index };
+    }
+  }
+};
+</script>
+
+<style>
+</style>
