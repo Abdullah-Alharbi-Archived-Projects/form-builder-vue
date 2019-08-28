3cbb9ffd0d266fc2c955ce4ce6326dbdf1098bf6
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 14:13:08 +0300

    deleteForm component

---
+++ src/components/DeleteForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click.prevent="deleteForm">Delete</button>
+</template>
+
+<script>
+export default {
+  name: "DeleteForm",
+  props: ["index"],
+  methods: {
+    deleteForm() {
+      this.$emit("delete-form", this.index);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
