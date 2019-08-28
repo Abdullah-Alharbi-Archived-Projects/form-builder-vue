a89306ff0216c2b5af2d472ce31104a72ca10723
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-28 13:58:22 +0300

    input component

---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <input :type="type" :placeholder="placeholder" v-model="model.object[getPropName]" />
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: ["model", "placeholder", "type"],
+  computed: {
+    getPropName() {
+      const { props, index } = this.model;
+      return props[index];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
